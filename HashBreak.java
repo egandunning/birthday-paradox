cf4889fdf15cacfe1f00429bff77f62cf8f1b7ae
Egan Dunning <dunninea@plu.edu>
2017-07-01 13:17:34 -0700

    added documentation to HashBreak.java

--- HashBreak.java
+++ HashBreak.java
@@ -1,15 +1,20 @@
 import java.util.Random;
 
+/**
+ * Finds a java hashCode collision using brute force. The output of hashCode is 
+ * a 32-bit integer.
+ */
 public class HashBreak {
 	
 	public static void main(String args[]) {
 		//Java hashcode() outputs a 32bit integer.
-		//By the birthday paradox, there is a 50% chance of finding a collision
-		//in 1.2*2^(n/2) hashes.
-		//2^16 = 65,536
+		//By the birthday paradox, there is a 50% chance of finding a hash function 
+		//collision in 1.2*2^(n/2) hashes where n is the number of bits in the hash
+		//function's output.
+		//1.2 * 2^16 = 1.2 * 65,536 = 78643.2
 		
-		String[] msg = new String[65536];
-		int[] hashes = new int[65536];
+		String[] msg = new String[78643];
+		int[] hashes = new int[78643];
 		
 		Random r = new Random();
 		int loopCounter = 1;
@@ -20,10 +25,11 @@ public class HashBreak {
 			
 			System.out.print("."); 
 			
-			for(int i = 0; i < 65536; i++) {
+			for(int i = 0; i < 78643; i++) {
 				
 				msg[i] = "";
 				
+				//random strings have 20 +/- 10 characters
 				for(int j = 0; j < r.nextInt(20) + 10; j++) {
 					// generate random string, hash the string
 					msg[i] += (char)(r.nextInt(74) + '0');
@@ -32,8 +38,10 @@ public class HashBreak {
 				}
 			}
 			
-			for(int i = 0; i < 65536; i++) {
-				for(int j = i + 1; j < 65536; j++) {
+			//check if any of the random strings hash to the same value
+			//by the birthday paradox, this should find a collision with .5 probability
+			for(int i = 0; i < 78643; i++) {
+				for(int j = i + 1; j < 78643; j++) {
 					if(hashes[i] == hashes[j]) {
 						System.out.println("\n" + loopCounter + " rounds to find a collision.");
 						System.out.println("Hash: " + hashes[i] + " PT: " + msg[i]);
