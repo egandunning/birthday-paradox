079e4af453310c6d7dec4fd63cbd88d20b5d1352
Egan Dunning <dunninea@plu.edu>
2017-07-03 18:04:17 -0700

    added comments to proof

--- proof.py
+++ proof.py
@@ -1,10 +1,12 @@
 #final step of proof for birthday paradox
+#Specific case- if 23 people are in a room, the probability
+#that at least 2 people share a birthday is 1/2.
 
 product = 1.0
 
 for i in range(23):
-	x = (365 - i) / 365.0
+	x = (365 - i) / 365.0 #the probability that i people dont share a birthday
 	print x
-	product = product * ((365 - i) / 365.0)
+	product = product * x
 
-print 1 - product
+print 1-product
